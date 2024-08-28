5bc519ad90f4dc06a6db09e210d1916cbb33497c
x130844 <73090986+x130844@users.noreply.github.com>
2024-08-28 16:33:20 -0500

    Update mgather.sh

--- mgather.sh
+++ mgather.sh
@@ -1,38 +1,65 @@
 #!/bin/bash
-#echo ""
-#echo "Mustgather Info about process: $1"
+#
+#
+sepline="\n"
+
+echo "Mustgather Info about process: \"$1\""
 if [ "$(pidof $1)" == "" ]
 then
-	echo "$1 process not found"
-	exit
+        echo "$1 process not found"
+        exit
 fi
 
-#echo $(pidof $1)
-for mypid in $(pidof $1); 
-do 
-	mypath="$(readlink /proc/$mypid/exe)\n$mypath"
-	mycwd="$(readlink /proc/$mypid/cwd)\n$mycwd"
-	#myenv="$(tr -d '\0' </proc/$mypid/environ)\n$myenv"
-	myenv="$(tr '\0' ' '</proc/$mypid/environ)\n$myenv"
-	mycmdline="$(tr '\0' ' ' </proc/$mypid/cmdline; echo)$mycmdline"
-	myulimit="$(tr '\0' '\n' </proc/$mypid/limits)\n$myulimit"
-#printf "$myulimit"
+for mypid in $(pidof $1);
+do
+        mypath="$(readlink /proc/$mypid/exe)\n$mypath"
+        mycwd="$(readlink /proc/$mypid/cwd)\n$mycwd"
+        #myenv="$(tr -d '\0' </proc/$mypid/environ)\n$myenv"
+        myenv="$(tr '\0' ' '</proc/$mypid/environ)\n$myenv"
+        mycmdline="$(tr '\0' ' ' </proc/$mypid/cmdline; echo)\n$mycmdline"
+        myulimit="$(tr '\0' '\n' </proc/$mypid/limits)\n$myulimit"
+        myowner="$(ps -o user= -p $mypid )\n$myowner"
 done
 
-echo ""
-echo "Actual Path:"
-printf "$mypath"|uniq
-echo ""
-echo "Current working directory:"
-printf "$mycwd"|uniq
-echo ""
-echo "Environment variables for that process:"
-printf "$myenv"|uniq
-echo ""
-echo "Command line arguments for that process:"
-echo "$mycmdline"|uniq
-echo ""
-echo "ulimits values for that process:"
-printf "$myulimit"|sort|uniq
-echo ""
+printf "$sepline"
 
+echo "Actual binary Path(s) for $1:"
+printf "$mypath"|uniq
+
+printf "$sepline"
+
+echo "Current working dir(s) for $1 :"
+printf "$mycwd"|uniq
+
+printf "$sepline"
+
+echo "Environment variables for $1:"
+printf "$myenv"|uniq
+
+printf "$sepline"
+
+echo "Command line arguments for $1:"
+printf "$mycmdline"|sort|uniq
+
+printf "$sepline"
+
+echo "ulimits values for $1:"
+printf "$myulimit"|sort|uniq
+
+printf "$sepline"
+
+echo "Server is running:" $(grep PRETTY_NAME /etc/os-release | cut -f 2 -d "=")
+
+if ! command -v sestatus &> /dev/null
+then
+        echo "Checked for SELinux, but not applicable here"
+else
+        sestatus
+fi
+
+printf "$sepline"
+
+echo "Owner(s) for $1 session(s):"
+printf "$myowner"|sort|uniq
+
+printf "$sepline"
